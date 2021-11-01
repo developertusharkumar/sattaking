b134b133842eff33f8e5fe2d994e125dbcd5440a
Tushar kumar <developer.tusharkumar@gmail.com>
2021-11-01 14:23:27 +0530

    [NEW] added the new changes for the result pagbe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,23 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBte6DrtujBVh21woJwgB7Ek7dTAcdaSmw',
-    authDomain: 'sattaking-official.firebaseapp.com',
-    databaseURL:
-      'https://sattaking-official-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'sattaking-official',
-    storageBucket: 'sattaking-official.appspot.com',
-    messagingSenderId: '109145097285',
-    appId: '1:109145097285:web:76bc2d7692a1ab6eb2cf2c',
-    measurementId: 'G-9DEEX37WXD',
+    // apiKey: 'AIzaSyBte6DrtujBVh21woJwgB7Ek7dTAcdaSmw',
+    // authDomain: 'sattaking-official.firebaseapp.com',
+    // databaseURL:
+    //   'https://sattaking-official-default-rtdb.asia-southeast1.firebasedatabase.app',
+    // projectId: 'sattaking-official',
+    // storageBucket: 'sattaking-official.appspot.com',
+    // messagingSenderId: '109145097285',
+    // appId: '1:109145097285:web:76bc2d7692a1ab6eb2cf2c',
+    // measurementId: 'G-9DEEX37WXD',
+    apiKey: "AIzaSyAxeXoh1ugfL4LWR3SOhiucsQcRGqyGFTM",
+    authDomain: "testing-sattaking.firebaseapp.com",
+    databaseURL: "https://testing-sattaking-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "testing-sattaking",
+    storageBucket: "testing-sattaking.appspot.com",
+    messagingSenderId: "765939966493",
+    appId: "1:765939966493:web:3fcc353baf13654d369cf4",
+    measurementId: "G-6WGF04YDSR"
   },
 };
 
